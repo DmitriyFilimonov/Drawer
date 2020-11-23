f2b291d33508cc026574e317ddd4e3e4a431cdd4
Dmitriy <banodannon@gmail.com>
2020-11-23 18:30:24 +0300

    start

---
+++ Drawer/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drawer
+{
+    public partial class Form1 : Form
+    {
+        Bitmap mainBitmap;
+        Graphics graphics;
+        Pen pen;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mainBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+    }
+}
