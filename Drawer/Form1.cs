2671ce95784bb822dc1acf41e0085c298c4a51d3
Dmitriy <banodannon@gmail.com>
2020-11-24 16:32:16 +0300

    brushStarted

--- Drawer/Form1.cs
+++ Drawer/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,9 @@ namespace Drawer
 {
     public partial class Form1 : Form
     {
-        Bitmap mainBitmap;
-        Graphics graphics;
-        Pen pen;
+        Bitmap _mainBitmap;
+        Graphics _graphics;
+        Pen _pen;
         
         public Form1()
         {
@@ -24,12 +25,21 @@ namespace Drawer
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            mainBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-            pictureBox1.Image = mainBitmap;
-            mainBitmap.SetPixel(50, 56, Color.Red);
-            mainBitmap.SetPixel(51, 56, Color.Red);
-            mainBitmap.SetPixel(50, 57, Color.Red);
-            mainBitmap.SetPixel(51, 57, Color.Red);
+            _mainBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            _graphics = Graphics.FromImage(_mainBitmap);
+            _pen = new Pen(Color.Red, 10);
+            //pictureBox1.Image = _mainBitmap;
+            
+            
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point _oldPoint = e.Location;
+            _oldPoint.X++;
+            _graphics.DrawLine(_pen, _oldPoint, e.Location);
+            pictureBox1.Image = _mainBitmap;
         }
     }
 }
